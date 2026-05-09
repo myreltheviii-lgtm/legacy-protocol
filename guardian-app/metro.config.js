915bf7294c77f904f510635f2ff84ba71f036529
myreltheix-lgtm <myreltheviii@gmail.com>
2026-05-09 21:15:44 +0000

    fix: add Node.js polyfills for assert, http, https, os, url in Metro config

--- guardian-app/metro.config.js
+++ guardian-app/metro.config.js
@@ -6,6 +6,11 @@ config.resolver.extraNodeModules = {
   crypto: require.resolve("expo-crypto"),
   stream: require.resolve("stream-browserify"),
   buffer: require.resolve("buffer"),
+  assert: require.resolve("assert"),
+  http: require.resolve("stream-http"),
+  https: require.resolve("https-browserify"),
+  os: require.resolve("os-browserify/browser"),
+  url: require.resolve("url"),
 };
 
 module.exports = config;
