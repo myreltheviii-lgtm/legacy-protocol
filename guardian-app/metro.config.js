0337dd513d2f020bdc5b8e0e7a736438c74e5bbb
myreltheix-lgtm <myreltheviii@gmail.com>
2026-05-09 21:06:08 +0000

    fix: use extraNodeModules for crypto/stream/buffer polyfills in Metro config

--- guardian-app/metro.config.js
+++ guardian-app/metro.config.js
@@ -2,8 +2,10 @@ const { getDefaultConfig } = require("expo/metro-config");
 
 const config = getDefaultConfig(__dirname);
 
-config.resolver.alias = {
+config.resolver.extraNodeModules = {
   crypto: require.resolve("expo-crypto"),
+  stream: require.resolve("stream-browserify"),
+  buffer: require.resolve("buffer"),
 };
 
 module.exports = config;
